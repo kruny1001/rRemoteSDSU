4877af864e572e8de21fe3c83875a394478bdec1
Kevin Son <kruny1001@gmail.com>
2015-08-10 14:07:38 -0500

    r package update

--- R/test.R
+++ R/test.R
@@ -2,7 +2,7 @@ library(jsonlite)
 
 #parse input from JSON into R
 test <- '{"x" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]}'
-fnargs <- fromJSON(jsoninput)
+fnargs <- fromJSON(test)
 
 #the actual function call
 result <- do.call(stats::sd, fnargs)
