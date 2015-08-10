5960f163fa88453055ad48d8cb12469f4a702ba1
Kevin Son <kruny1001@gmail.com>
2015-08-10 14:02:32 -0500

    r package update

--- R/test.R
+++ R/test.R
@@ -1,7 +1,7 @@
 library(jsonlite)
 
 #parse input from JSON into R
-jsoninput <- '{"x" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]}'
+test <- '{"x" : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]}'
 fnargs <- fromJSON(jsoninput)
 
 #the actual function call
