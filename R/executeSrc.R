115e8e3de5cf426ebc786c8bf05c4c635e997c86
Kevin Son <kruny1001@gmail.com>
2015-08-10 14:34:55 -0500

    r package update

---
+++ R/executeSrc.R
@@ -0,0 +1,8 @@
+make_chart <- function(text){
+  writeLines(text, con="input.R")
+  chart = source('input.R', local = TRUE)$value
+  # chart$set(width = 700)
+  # chart$setTemplate(page = 'rChart2.html')
+  chart$save('output.html', cdn = TRUE)
+  invisible();
+}
