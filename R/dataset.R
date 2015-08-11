2652cb5450f8029d4dba98146ff801c6c33a25a8
Kevin Son <kruny1001@gmail.com>
2015-08-11 14:50:38 -0500

    runSrc

---
+++ R/dataset.R
@@ -0,0 +1,9 @@
+make_chart <- function(text){
+  writeLines(text, con="input.R")
+  chart = source('input.R', local = TRUE)$value
+  # chart$set(width = 700)
+  # chart$setTemplate(page = 'rChart2.html')
+  chart$save('output.html', cdn = TRUE)
+  invisible();
+}
+
--- R/executeSrc.R
+++ R/executeSrc.R
@@ -1,8 +1,5 @@
-executeSrc <- function(text){
+runSrc <- function(text){
   writeLines(text, con="input.R")
-  chart = source('input.R', local = TRUE)$value
-  # chart$set(width = 700)
-  # chart$setTemplate(page = 'rChart2.html')
-  chart$save('output.html', cdn = TRUE)
-  invisible();
+  chart = source('input.R', local = TRUE)
+  return(chart)
 }
