cdde7920a571702c14d52dc51fad5d27450e75a3
Kevin Son <kruny1001@gmail.com>
2015-08-11 16:19:48 -0500

    meanJson

--- R/meanJson.R
+++ R/meanJson.R
@@ -1,7 +1,7 @@
 
 
 library(jsonlite)
-meanJson <- function(text){
+meanJson <- function(){
 args <- fromJSON('{"n":3, "mean": 10, "sd":10}')
 output <- do.call(stats::rnorm, args)
 result(toJSON(output))
