dddd803e35b0100bd3553e47406c7c8d28e2dd65
Kevin Son <kruny1001@gmail.com>
2015-09-18 18:01:43 -0500

    update R pacakges

---
+++ R/highlowplot.R
@@ -0,0 +1,14 @@
+#' High/Low Plot
+#' 
+#' Creates a high/low plot of stock data.
+#' 
+#' @param ticker stock ticker symbol. E.g. "GOOG". 
+#' @param from start date. Either string or date object.
+#' @param to end date. Either string or date object.
+#' @return ggplot object.
+#' @export
+highlowplot <- function(ticker = "GOOG", from = "2013-01-01", to=Sys.time()){
+  mydata <- yahoodata(ticker, from, to);
+  mydata$up <- mydata$Open < mydata$Close;
+  ggplot(data=mydata, aes(Date, Close, ymin=Low, ymax=High, color=up)) + geom_linerange() + theme(legend.position="none");
+}
--- R/smoothplot.R
+++ R/smoothplot.R
@@ -7,6 +7,7 @@
 #' @param to end date. Either string or date object.
 #' @return ggplot object.
 #' @export
+#' 
 smoothplot <- function(ticker = "GOOG", from = "2013-01-01", to=Sys.time()){
   mydata <- yahoodata(ticker, from, to);
   qplot(Date, Close, data = mydata, geom = c("line", "smooth"));  
