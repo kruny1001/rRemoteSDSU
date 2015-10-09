83961c731a421c233d85bf27ee3a46d070157f38
Kevin Son <kruny1001@gmail.com>
2015-10-09 14:36:21 -0500

    10/9

--- R/scan.R
+++ R/scan.R
@@ -1,21 +1,15 @@
-
 #' scan 
 #' 
 #' This is a wrapper for the OpenCPU application. It is a single function that calls out to various plot types.
 #' The function prints a plot to the graphics device and returns nothing.
 #' 
-#' @param type type of plot to create. One of "smoothplot", "highlowplot", "areaplot".
-#' @param ticker stock ticker symbol. E.g. "GOOG".
-#' @param from start date. Either string or date object.
-#' @param to end date. Either string or date object.
-#' @param current include the current price of this stock. TRUE/FALSE.
 #' @export
 #' @import rjson
 #' 
 #' 
 #' 
 #' 
-scanR <- function(ticker = "GOOG", from = "2013-01-01", to=Sys.time()){
+scanR <- function(){
   json_file <- "https://statsdsu.firebaseio.com/data1.json"
   json_data <- fromJSON(file=json_file)
   hist(json_data)
