9bb7beb0530f408c392a0241f0675198507f0620
Kevin Son <kruny1001@gmail.com>
2016-06-18 22:09:27 -0500

    a

---
+++ R/socket.R
@@ -0,0 +1,12 @@
+#' 1.2 socket
+#' 
+#' Returns a row with the latest price for a given stock.
+#' 
+#' @param text source code of R. Required.
+#' @return ggplot object.
+#' @export
+#' 
+
+server <- function(){
+  return(TRUE)
+}
